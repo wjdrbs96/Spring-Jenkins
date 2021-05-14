b807eb2c231d6263b4c7caedfafb729587c55ba4
wjdrbs96 <wjdrbs966@naver.com>
2021-05-14 16:11:18 +0900

    Jenkin CI/CD

---
+++ src/main/java/com/example/jenkins/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by jg 2021/05/14
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello DDD";
+    }
+}
